fe3497787e4dafcc73dc4e91becb3c7e9bf1fd9d
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2026-01-19 19:36:02 +0900

    feat: support polyfill
    https://developer.chrome.com/blog/geolocation-html-element#polyfill

---
+++ src/types/geolocation-element-polyfill.d.ts
@@ -0,0 +1,9 @@
+declare module 'geolocation-element-polyfill' {
+	/**
+	 * Polyfill for the <geolocation> HTML element.
+	 * When imported, it automatically registers <geolocation> or <geo-location>
+	 * as a custom element backed by the standard Geolocation API.
+	 */
+	const value: unknown
+	export default value
+}
