b077186be36a6edc7da6f17029d37608d4e709b8
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2026-01-19 18:47:42 +0900

    feat: add onLocation event handling to GeoLocation
    https://developer.chrome.com/blog/geolocation-html-element#implementation

--- test/ui/app.tsx
+++ test/ui/app.tsx
@@ -2,9 +2,21 @@ import GeoLocation from '../../src'
 
 export function App() {
 	return (
-		<main>
-			<GeoLocation />
-		</main>
+		<GeoLocation
+			autolocate
+			accuracymode="precise"
+			watch
+			onLocation={(
+				position: GeolocationPosition | null,
+				err?: GeolocationPositionError | null,
+			) => {
+				if (position) {
+					console.log(position)
+				} else if (err) {
+					console.error(err)
+				}
+			}}
+		/>
 	)
 }
 
