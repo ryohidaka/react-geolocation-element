a90e6cdd0d3419b9d268e8ea5c79107fd061d540
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2026-01-19 18:17:55 +0900

    Initial commit from Bunup

---
+++ src/components/button.tsx
@@ -0,0 +1,3 @@
+export function Button(props: React.ComponentProps<'button'>): React.ReactNode {
+	return <button type="button" data-slot="button" {...props} />
+}
---
+++ test/ui/index.tsx
@@ -0,0 +1,10 @@
+import { serve } from 'bun'
+import index from './index.html'
+
+serve({
+	routes: {
+		'/*': index,
+	},
+})
+
+console.log('🚀 Server running at http://localhost:3000')
